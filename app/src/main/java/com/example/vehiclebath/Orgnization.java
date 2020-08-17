7f177bf93849e0cf699a1023681560fe031d54ef
hillaryjeramin@gmail.com <hillaryjeramin@gmail.com>
2020-08-17 09:45:37 +0530

    Initial Commit Radesh

---
+++ app/src/main/java/com/example/vehiclebath/Orgnization.java
@@ -0,0 +1,16 @@
+package com.example.vehiclebath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Orgnization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orgnization);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
---
+++ app/src/main/java/com/example/vehiclebath/organization_report_customer.java
@@ -0,0 +1,21 @@
+package com.example.vehiclebath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class organization_report_customer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_report_customer);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+    }
+}
