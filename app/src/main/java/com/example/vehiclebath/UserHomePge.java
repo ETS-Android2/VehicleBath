02fe74c8362145d604bb79ea6981046749b23ed6
Viraj <avjoachim@hotmail.com>
2020-09-26 23:07:05 +0530

    joachim commited2

--- app/src/main/java/com/example/vehiclebath/UserHomePge.java
+++ app/src/main/java/com/example/vehiclebath/UserHomePge.java
@@ -8,13 +8,32 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+
+import io.paperdb.Paper;
 
 public class UserHomePge extends AppCompatActivity {
+
+    private Button logoutbtn1;
+    private DatabaseReference AdvertisementRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home_pge);
+
+        logoutbtn1 = (Button) findViewById(R.id.logout1);
+
+        logoutbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Paper.book().destroy();
+
+                Intent intent = new Intent(UserHomePge.this,LoginNew.class);
+                startActivity(intent);
+            }
+        });
 
         Intent i = getIntent();
 
