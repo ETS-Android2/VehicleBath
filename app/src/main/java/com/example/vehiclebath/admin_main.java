0ae217e9247f2966914169c878600dc762abad6a
hillaryjeramin@gmail.com <hillaryjeramin@gmail.com>
2020-10-04 20:31:21 +0530

    admin_main_changed_Commit_with_Subscriptionbtn_Added

---
+++ app/src/main/java/com/example/vehiclebath/admin_main.java
@@ -0,0 +1,79 @@
+package com.example.vehiclebath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class admin_main extends AppCompatActivity {
+
+    Button cusbutton,userBlackbutton,addButton,subButton,repButton,adminRatingsBtn,btnAppointments;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+
+//        orgbutton = findViewById(R.id.btnOrganization);
+        cusbutton =  findViewById(R.id.btnCustomer);
+        userBlackbutton = findViewById(R.id.btnUserBlack);
+        addButton = findViewById(R.id.btnAdd);
+        subButton = findViewById(R.id.btnSub);
+        repButton = findViewById(R.id.btnReoprtCustomer);
+        adminRatingsBtn = findViewById(R.id.btnAdminRatings);
+        btnAppointments = findViewById(R.id.btnAppointments);
+
+
+
+//        orgbutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent =  new Intent(admin_main.this,Orgnization.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        cusbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(admin_main.this,customers.class);
+                startActivity(intent);
+            }
+        });
+
+        userBlackbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(admin_main.this,CustomerReportedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        repButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(admin_main.this,CustomerReportedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        adminRatingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(admin_main.this,AdminRating.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnAppointments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(admin_main.this,AppointmentbtnActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
