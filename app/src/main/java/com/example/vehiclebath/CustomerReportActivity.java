3d32e5526117f768c029e67bbe9735e5270acb5d
hillaryjeramin@gmail.com <hillaryjeramin@gmail.com>
2020-10-04 20:33:22 +0530

    Removed_sample_comments

--- app/src/main/java/com/example/vehiclebath/CustomerReportActivity.java
+++ app/src/main/java/com/example/vehiclebath/CustomerReportActivity.java
@@ -32,7 +32,7 @@ import java.util.HashMap;
 
 public class CustomerReportActivity extends AppCompatActivity {
 
-    //private Button CreateCustomerReport;
+
     private TextView reportName,reportPhone,reportPassword,reportEmail;
     private ProgressDialog loadingBar;
 
@@ -47,26 +47,7 @@ public class CustomerReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_report);
-        //setContentView(R.layout.customer_list_report_layout);
 
-/*
-        CreateCustomerReport = (Button)findViewById(R.id.btnReportSubmit);
-        reportName = (TextView)findViewById(R.id.user_name_report);
-        reportPhone = (TextView)findViewById(R.id.user_phone_report);
-        reportPassword = (TextView)findViewById(R.id.user_password_report);
-        reportEmail = (TextView)findViewById(R.id.user_email_report);
-
-
-        CreateCustomerReport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-*/
-
-        //-----------------------------------------------------//
 
         UserRef = FirebaseDatabase.getInstance().getReference().child("UsersReport");
 
@@ -80,85 +61,7 @@ public class CustomerReportActivity extends AppCompatActivity {
 
     }
 
-    //----------------------------------------------------------------------------------------//
 
-  /*  private void CreateReport()
-    {
-        String userName =reportName.getText().toString();
-        String userPhone =reportPhone.getText().toString();
-        String userPassword =reportPassword.getText().toString();
-        String userEmail =reportEmail.getText().toString();
-
-        loadingBar.setTitle("Report Submission Progressing");
-        loadingBar.setMessage("Please wait we are checking for you credentials");
-        loadingBar.setCanceledOnTouchOutside(false);
-        loadingBar.show();
-
-
-        validate(userName,userPhone,userPassword,userEmail);
-
-    }
-
-    private void validate(final String userName,final  String userPhone,final  String userPassword,final  String userEmail) {
-
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
-
-        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!(dataSnapshot.child("UsersReport").child(userPhone).exists()))
-                {
-                    HashMap<String,Object> userDataMap = new HashMap<>();
-
-                    userDataMap.put("Phone",userPhone);
-                    userDataMap.put("Password",userName);
-                    userDataMap.put("Name",userPassword);
-                    userDataMap.put("Email",userEmail);
-
-                    RootRef.child("UsersReport").child(userPhone).updateChildren(userDataMap)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(CustomerReportActivity.this, "Report has been submitted successfully", Toast.LENGTH_SHORT).show();
-                                        loadingBar.dismiss();
-
-                                       // Intent intent = new Intent(CustomerReportActivity.this,LoginActivity.class);
-                                        // startActivity(intent);
-
-                                    }
-                                    else{
-                                        loadingBar.dismiss();
-                                        Toast.makeText(CustomerReportActivity.this, "Network Error : Please try again after some time", Toast.LENGTH_SHORT).show();
-
-                                    }
-                                }
-                            });
-
-
-                }
-                else
-                {
-                    Toast.makeText(CustomerReportActivity.this, "This "+userPhone+" number already exist", Toast.LENGTH_SHORT).show();
-                    loadingBar.dismiss();
-                    Toast.makeText(CustomerReportActivity.this,"There is already a report been submitted on this number!",Toast.LENGTH_SHORT).show();
-
-
-                    //Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
-                    //startActivity(intent);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-*/
-    //----------------------------------------------------------------------------------------//
 
     @Override
     protected void onStart() {
